d41fcb61cbc3090f82bd0e227cf39e89ae739dd6
Christian Santos <cesantos647@gmail.com>
2023-07-13 19:52:43 -0700

    Update home page layout

--- pages/index.js
+++ pages/index.js
@@ -12,50 +12,40 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          My Name Is <a href="https://linkedin.com/in/cesantos647/">Christian Santos</a>
+          I'm <a href="https://linkedin.com/in/cesantos647/">Christian Santos</a>
         </h1>
 
         <p className={styles.description}>
-          Working as a <code>Software Engineer - Data Analytics</code>
+          {/*<code>Software Engineer - Data Analytics</code>*/}
+          <code>Software Engineer - Data</code>
+          {/* Software Engineer - Data */}
+
         </p>
 
         <div className={styles.grid}>
-          <Link href="/about/me" className={styles.card}>
-            <h3>About Me &rarr;</h3>
-            <p>Not sure what I should put here, but I might want to do something since it feels wierd to only have 2 things</p>
-          </Link>
-
+          
           <Link href="/about/resume/" className={styles.card}>
             <h3>My Resume &rarr;</h3>
-            <p>My professional work experience: Past, Present, Future(?)</p>
           </Link>
 
           <Link href="/about/portfolio/" className={styles.card}>
             <h3>Portfolio &rarr;</h3>
-            <p>Discover what I like to create outside of work</p>
           </Link>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Github / Linkedin &rarr;</h3>
-            <p>
-              I feel like this would be goot to embed somewhere, but I don't have to put it front and center
-            </p>
-          </a>
-        </div>
+        </div> 
       </main>
 
       <footer>
-        <a
+        {/* <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}, Might want to put icons for all my stuff here and not put them at the very top
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+        </a> */}
+        <a>Github /</a>
+        <a>/ LinkedIn</a>
       </footer>
 
       <style jsx>{`
@@ -86,10 +76,11 @@ export default function Home() {
           color: inherit;
         }
         code {
-          background: #fafafa;
+          background: hsl(0, 12%, 80%);
+          color: hsl(260, 30%, 40%);
           border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
+          padding: 1rem;
+          font-size: 2.3rem;
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
